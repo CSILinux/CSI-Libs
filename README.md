957f9a551505c86e6ab6a59386106863f4e65fcb
Pak Cyberbot <93365275+PakCyberbot@users.noreply.github.com>
2023-09-03 15:22:08 +0000

    Updated data module for handling json as dict
    apiKeys:
    - get_dict() to get dict of data
    - apiKeys['key'] to assign and get specific value
    agencyData
    - same as above
    
    added User_Agents.json too

---
+++ README.md
@@ -0,0 +1,6 @@
+# Under Development
+
+Currently, @pakcyberbot is assigned to this project. If you have any queries or would like to contribute, please feel free to contact me.
+
+### Purpose
+The purpose of this project is to facilitate the development of CSI tools. It includes all the necessary code blocks, as well as assets such as PNG images and data files. Everything is categorized into separate modules.
--- csilibs/data/__init__.py
+++ csilibs/data/__init__.py
@@ -1,8 +1,8 @@
-import sqlite3, json, os
+import sqlite3, json, os, copy
 
 _abs_path = os.path.abspath(os.path.dirname(__file__))  
 
-class Sites_User:
+class SitesUser:
     SITES_SOCIAL = "Social_sites"
     SITES_NSFW = "NSFW_sites"
     SITES_ONION = "Onion_sites"
@@ -13,7 +13,7 @@ class Sites_User:
     
     @staticmethod
     def get_data(sites_category=SITES_SOCIAL):   
-        with sqlite3.connect(Sites_User.db_filename) as conn:
+        with sqlite3.connect(SitesUser.db_filename) as conn:
             cursor = conn.cursor()
             
             cursor.execute(f"SELECT * FROM {sites_category}")
@@ -29,3 +29,44 @@ class Sites_User:
             return result
     # Later implement write functionality too
     
+
+class __JsonData:
+    def __init__(self, filename):
+        self.file_path = os.path.join(_abs_path,filename)
+        try:
+            with open(self.file_path, 'r') as file:
+                self.data = json.load(file)
+        except FileNotFoundError:
+            return None
+    
+    def get_dict(self):
+        return self.data
+
+    def __getitem__(self, key):
+        return self.data[key]
+    
+    def __setitem__(self, key, value):
+        self.data[key] = value
+        try:
+            with open(self.file_path, 'w') as file:
+                json.dump(self.data, file, indent=4)
+            return True
+        except Exception as e:
+            return False
+        
+    
+    def set_dict(self, data):
+        self.data = data
+        try:
+            with open(self.file_path, 'w') as file:
+                json.dump(self.data, file, indent=4)
+            return True
+        except Exception as e:
+            return False
+        
+    def __str__(self) -> str:
+        return repr(self.data)
+
+    
+agencyData = __JsonData('agency_data.json')
+apiKeys = __JsonData('APIKeys.json')
