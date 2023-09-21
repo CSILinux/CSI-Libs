16291d786277c910dffd884b0c90750ea97dcae3
Pak Cyberbot <93365275+PakCyberbot@users.noreply.github.com>
2023-09-21 15:50:06 +0000

    Added docx&odt templates and sitesuser add newdata

--- csilibs/data/__init__.py
+++ csilibs/data/__init__.py
@@ -1,13 +1,30 @@
 import sqlite3, json, os, copy
+from typing import TypedDict
+
+class SitesDict(TypedDict):
+    site_name: str
+    site_url: str
+    method: str
+    type: str
+    key: str
+    onoff: str
 
 _abs_path = os.path.abspath(os.path.dirname(__file__))  
 
+class Templates:
+    DOCX_CSI_TEMPLATE = os.path.join(_abs_path,'Templates','csi-template.docx')
+    ODT_CSI_TEMPLATE = os.path.join(_abs_path,'Templates','csi-template.odt')
+    ODT_CSI_MISSING_PERSON = os.path.join(_abs_path,'Templates','CSI-Missing-Persons-Template.odt')
+    ODT_DIGITAL_EVIDENCE_FORENSIC = os.path.join(_abs_path,'Templates','Digital-Evidence-Forensic-Report-Template.odt')
+    ODT_CONSENT_TO_SEARCH = os.path.join(_abs_path,'Templates','Consent-to-Search.odt')
+    
 class SitesUser:
     SITES_SOCIAL = "Social_sites"
     SITES_NSFW = "NSFW_sites"
     SITES_ONION = "Onion_sites"
     SITES_FIN = "Financial_sites"
     SITES_GAMING = "Gaming_sites"
+    SITES_SAFE_LIST = [SITES_FIN,SITES_GAMING,SITES_GAMING,SITES_SOCIAL]
 
     db_filename = os.path.join(_abs_path,'sites_userenum.db')
     
@@ -27,6 +44,27 @@ class SitesUser:
             # Convert the rows to dictionaries
             result = [row_to_dict(row, cursor) for row in rows]
             return result
+    
+    
+    def add_data(sites_category, data: SitesDict):
+        if not all(key in data and isinstance(data[key], str) for key in SitesDict.__annotations__):
+            raise ValueError("Invalid 'data' dictionary format. It should match the 'SitesDict' format.\n SitesDict = {'site_name': str, 'site_url': str, 'method': str, 'type': str, 'key': str, 'onoff': str }")
+        
+        with sqlite3.connect(SitesUser.db_filename) as conn:
+            cursor = conn.cursor()
+
+            # Prepare the SQL query to insert data
+            insert_query = f"INSERT INTO {sites_category} ({', '.join(data.keys())}) VALUES ({', '.join(['?']*len(data))})"
+            
+            try:
+                # Execute the query with the values from the 'data' dictionary
+                cursor.execute(insert_query, tuple(data.values()))
+                conn.commit()
+                print("Data added successfully.")
+            except sqlite3.Error as e:
+                conn.rollback()
+                raise e
+        
     # Later implement write functionality too
     
 
