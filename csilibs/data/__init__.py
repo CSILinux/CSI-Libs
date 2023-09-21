29bc5737a3d15de39cfd02a6b581af354542e1ac
Pak Cyberbot <93365275+PakCyberbot@users.noreply.github.com>
2023-09-21 17:10:09 +0000

    improved encrypt, decrypt and apiKeys fetching
    no need to use encrypt, decrypt & gen_key. all the features are in one apiKeys(). read its docstring for its usage

--- csilibs/data/__init__.py
+++ csilibs/data/__init__.py
@@ -1,5 +1,9 @@
 import sqlite3, json, os, copy
 from typing import TypedDict
+from csilibs.auth import gen_key, encrypt, decrypt
+
+_abs_path = os.path.abspath(os.path.dirname(__file__))  
+
 
 class SitesDict(TypedDict):
     site_name: str
@@ -8,8 +12,7 @@ class SitesDict(TypedDict):
     type: str
     key: str
     onoff: str
-
-_abs_path = os.path.abspath(os.path.dirname(__file__))  
+    
 
 class Templates:
     DOCX_CSI_TEMPLATE = os.path.join(_abs_path,'Templates','csi-template.docx')
@@ -17,6 +20,7 @@ class Templates:
     ODT_CSI_MISSING_PERSON = os.path.join(_abs_path,'Templates','CSI-Missing-Persons-Template.odt')
     ODT_DIGITAL_EVIDENCE_FORENSIC = os.path.join(_abs_path,'Templates','Digital-Evidence-Forensic-Report-Template.odt')
     ODT_CONSENT_TO_SEARCH = os.path.join(_abs_path,'Templates','Consent-to-Search.odt')
+    
     
 class SitesUser:
     SITES_SOCIAL = "Social_sites"
@@ -107,4 +111,51 @@ class __JsonData:
 
     
 agencyData = __JsonData('agency_data.json')
-apiKeys = __JsonData('APIKeys.json')
+
+__api_json = os.path.join(_abs_path, 'APIKeys.json')
+__api_enc = os.path.join(_abs_path, 'APIKeys.enc')
+
+        
+def apiKeys(password='', data={}):
+    """Returns a dictionary containing API keys.
+If an encrypted file is present and a password is provided, it decrypts the file.
+If no enc file is present, it encrypts the API keys if a password is provided, but this operation does not return any data.
+If no password is provided, it simply returns the data.
+
+If a 'data' dictionary argument is provided, it will overwrite the file with the data in JSON format.
+
+    Args:
+        password (str, optional): takes password. Defaults to ''.
+        data (dict, optional): takes api keys dict to save. Defaults to {}.
+        
+    Raises:
+        ValueError: when encrypted file available and no password given
+
+    Returns:
+        tuple: is_encrypted_file_already_availabe bool , ApiKeys_data dict
+    """
+    enc_file = False
+    if os.path.exists(__api_enc):
+        enc_file = True
+        if password == '':
+            return enc_file, {}
+            # raise ValueError("password as an argument should be given to apiKeys to decrypt APIKeys.enc")
+        
+        if not decrypt(gen_key(password), __api_enc, __api_json):
+            raise ValueError("Decryption failed: A valid password is required to decrypt APIKeys.enc") 
+        
+        api_keys = __JsonData(__api_json)
+        
+        if data != {}:
+            api_keys.set_dict(data)
+            
+        encrypt(gen_key(password), __api_json, __api_enc)
+    
+    elif os.path.exists(__api_json):
+        if password == '':
+            api_keys = __JsonData(__api_json)
+        else:
+            encrypt(gen_key(password),__api_json,__api_enc)
+            return enc_file, {}
+            
+    return enc_file, api_keys.get_dict()
